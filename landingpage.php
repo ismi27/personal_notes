f325b8371507acbd3da9e44589192dbb3d0c3110
Cumeow <54826944+ismi27@users.noreply.github.com>
2021-06-29 20:01:17 +0700

    Initial commit

---
+++ landingpage.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project 3 - Ismi Rizki Sopiyanti</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="style-landingpage.css">
+</head>
+
+<body>
+    <nav class="navbar">
+        <h1>Personal Notes</h1>
+        <div class="nav-menu">
+        <a href="mainpage.php"><button class="btn" style="background: transparent; color: black">Home</button></a>
+            <div class="input-group">
+            <a href="aboutus.php"><button class="btn" style="background: transparent; color: black">About Us</button></a>
+            </div>
+            <a href="login.php"><button class="btn">Log In</button></a>
+        </div>
+
+    </nav>
+    <section class="hero-container">
+        <div>
+            <div>
+                <h1>Stay Productive
+                </h1>
+                <p>Take A Notes Everyday</p>
+            </div>
+            <section class="button-container">
+                <button class="btn" type="button" onclick="location.href='register.php';">Sign Up Now</button>
+            </section>
+        </div>
+    </section>
+    <section class="description-container">
+        <div>
+            <div>
+                <h2> Do it all with Personal Notes</h2>
+                <p>You can write notes according to categories that you can set yourself, making them easier to view with a simple display.....</p>
+            </div>
+            <div class="description-left-footer">
+                <p class="font-weight-bold">Read more</p>
+            </div>
+        </div>
+        <img src="note.png" alt="prototype">
+    </section>
+    <section class="content-container">
+        <div class="card">
+            <img src="note1.png" alt="card1">
+            <div class="card-body">
+                <h3>Get your personal task</h3>
+                <p>You can write all your stories, personal notes, cellphone numbers in this personal category, of course you can change the name....</p>
+            </div>
+            <div class="card-footer">
+
+                <p class="font-weight-bold">Read more</p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="note2.png" alt="card2">
+            <div class="card-body">
+                <h3>Do you have a new project?</h3>
+                <p>You can write a whole new project whether it's description, requirements, deadlines etc. in this project category, of course you can change the name....</p>
+            </div>
+            <div class="card-footer">
+
+                <p class="font-weight-bold">Read more</p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="note3.png" alt="card3">
+            <div class="card-body">
+                <h3>Write all done task</h3>
+                <p>You can write down all the notes that need to be corrected in an effort to complete this new project, etc. in this in progress category, of course you can change the name....</p>
+            </div>
+            <div class="card-footer">
+
+                <p class="font-weight-bold">Read more</p>
+            </div>
+        </div>
+    </section>
+    <section class="button-container">
+        <button class="btn">See More Now</button>
+    </section>
+    <footer>
+        <p><b>Personal Notes</b> 2021 copyright all rights reserved</p>
+    </footer>
+
+</body>
+
+</html>
---
+++ mainpage.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project 3 - Ismi Rizki Sopiyanti</title>
+    <link rel="stylesheet" href="style-mainpage.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="calendar">
+            <h1 style="letter-spacing: 4px; padding-top: 30px; font-weight: bold; color:#355f64;">M O N T H | Y E A R</h1>
+            <br>
+            <h3>S M T W T F S</h3>
+            <p style="letter-spacing: 14px;">1 2 3 </p>
+            <p><span style="letter-spacing: 14px;">4 5 6 7 8 9</span><span style="letter-spacing: 10px;"> </span><span style="padding-right: 10px;">10</span></p>
+            <p>11<span style="letter-spacing: 23px;"> </span>12<span style="letter-spacing: 23px;"> </span>13<span style="letter-spacing: 23px;"> </span>14<span style="letter-spacing: 23px;"> </span>15<span style="letter-spacing: 22px;"> </span>16<span style="letter-spacing: 20px;"> </span>17<span style="letter-spacing: 14px; padding-right: 10px;"> </span></p>
+            <p>18<span style="letter-spacing: 23px;"> </span>19<span style="letter-spacing: 23px;"> </span>20<span style="letter-spacing: 23px;"> </span>21<span style="letter-spacing: 23px;"> </span>22<span style="letter-spacing: 21px;"> </span>23<span style="letter-spacing: 20px;"> </span>24<span style="letter-spacing: 14px; padding-right: 10px;"> </span></p>
+            <p>25<span style="letter-spacing: 23px;"> </span>26<span style="letter-spacing: 23px;"> </span>27<span style="letter-spacing: 23px;"> </span>28<span style="letter-spacing: 21px;"> </span>29<span style="letter-spacing: 23px;"> </span>30<span style="letter-spacing: 21px;padding-right: 25px;"> </span> <span style="letter-spacing: 34px; padding-right: 20px;"> </span></p>
+        </div>
+        <div class="personal">
+            <button class="btn">
+                <h1 onclick="location.href='register.php';">P E R S O N A L</h1>
+            </button>
+
+        </div>
+        <div class="gttt">
+            <h1 style="color: #355f64; font-weight: bold; font-size: 22pt; letter-spacing: 8px; padding-top: 10px;"">GOOD</h1>
+            <h1 style=" color: #355f64; font-weight: bold; font-size: 22pt; letter-spacing: 8px; padding-top: 0px;">
+                THINGS</h1>
+            <h1 style="color: #355f64; font-weight: bold; font-size: 22pt; letter-spacing: 8px; padding-top: 0px;">TAKE
+                TIME</h1>
+        </div>
+        <div class="projects">
+            <button class="btn">
+                <h1 style="color: #fdfefe; font-weight: normal;padding-top: 10px;">P R O J E C T S</h1>
+            </button>
+        </div>
+        <div class="inprogress">
+            <button class="btn">
+                <p class="pp">I N<span style="letter-spacing: 15px;"> </span>P R O G R E S S</p>
+            </button>
+        </div>
+        <div class="work">
+            <button class="btn">
+                <h1 style="color: #fdfefe; font-weight: normal;padding-top: 10px;">W O R K</h1>
+            </button>
+        </div>
+        <div class="finished">
+            <button class="btn">
+                <h1 style="color: #fdfefe; font-weight: normal;padding-top: 10px;">F I N I S H E D</h1>
+            </button>
+        </div>
+        <noscript>You need to enable JavaScript to view the full site.</noscript>
+
+
+    </div>
+</body>
+
+</html>
